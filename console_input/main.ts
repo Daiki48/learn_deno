a1dc1828e5bd014d8a04f943031f4ce89ce67b01
Daiki48 <daiki48.engineer@gmail.com>
2022-10-26 14:11:39 +0900

    [add] console input

---
+++ console_input/main.ts
@@ -0,0 +1,11 @@
+const hello = "hello";
+console.log(hello);
+
+const input = prompt('Please enter input');
+console.log(input);
+
+if (input == 'world') {
+  console.log(hello + input);
+} else {
+  console.log('false: ' + input);
+}
